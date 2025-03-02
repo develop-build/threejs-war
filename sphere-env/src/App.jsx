52e0bd6fe208b90849e2b94514a16dabcbb22275
Deepak <dpkkr2214@gmail.com>
2025-03-02 10:25:36 +0530

    sphere-texture-hdr

--- sphere-env/src/App.jsx
+++ sphere-env/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
 const App = () => {
   return (
+    <Canvas>
+      <Environment preset="sunset" background />
+      <Sphere />
+      <OrbitControls />
+    </Canvas>
+  )
+}
+
+
+const Sphere = () => { 
+
+  return (
     <>
-      
+      <ambientLight intensity={0.2} />
+      <directionalLight position={[5, 5, 5]} intensity={1}/>
+      <mesh position={[0, 1, 0]}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial metalness={1} roughness={0} />
+      </mesh>
     </>
   )
 }
